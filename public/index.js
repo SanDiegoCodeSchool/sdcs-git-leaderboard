229c788ee95a672684c0b2dc7e5ffa5e8b58ee7e
Michael Roberts Jr <mike@sdcs.io>
2019-12-08 17:29:52 -0800

    restores the index.js file

--- public/index.js
+++ public/index.js
@@ -1,16 +1,32 @@
-var competitors = [];
+var mockData = [
+    {
+        username: 'firstUser',
+        points: 5012
+    },
+    {
+        username: 'secondUser',
+        points: 9182
+    },
+    {
+        username: 'thirdUser',
+        points: 1736
+    }
+]
+
+var competitors = {};
 
 // TODO: this is a work in progress, should be able to add new users to the list to track
 function addUser() {
    var userInput = document.getElementById("user-input").value;
-   var userObject = {username: userInput}
-   competitors.push(userObject);
+   console.log(userInput)
+   userInput.appendTo("competitors");
+   console.log(competitors)
 };
 
 function populateTable(){
-    competitors.sort( (a, b) => b.points-a.points);
+    mockData.sort( (a, b) => b.points-a.points);
 
-    var sortedData = competitors.map((student, i) =>
+    var sortedData = mockData.map((student, i) =>
         `<tr>
             <td>${i+1}</td>
             <td>${student.username}</td>
