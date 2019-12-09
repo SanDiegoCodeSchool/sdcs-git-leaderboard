34305215f07492593e2ce5d46394a9064c82f2bb
javisosa <javi.sosa1617@gmail.com>
2019-12-08 17:22:53 -0800

    adds padding and fixes favicom created learning moments

--- public/index.js
+++ public/index.js
@@ -1,32 +1,16 @@
-var mockData = [
-    {
-        username: 'firstUser',
-        points: 5012
-    },
-    {
-        username: 'secondUser',
-        points: 9182
-    },
-    {
-        username: 'thirdUser',
-        points: 1736
-    }
-]
-
-var competitors = {};
+var competitors = [];
 
 // TODO: this is a work in progress, should be able to add new users to the list to track
 function addUser() {
    var userInput = document.getElementById("user-input").value;
-   console.log(userInput)
-   userInput.appendTo("competitors");
-   console.log(competitors)
+   var userObject = {username: userInput}
+   competitors.push(userObject);
 };
 
 function populateTable(){
-    mockData.sort( (a, b) => b.points-a.points);
+    competitors.sort( (a, b) => b.points-a.points);
 
-    var sortedData = mockData.map((student, i) =>
+    var sortedData = competitors.map((student, i) =>
         `<tr>
             <td>${i+1}</td>
             <td>${student.username}</td>
